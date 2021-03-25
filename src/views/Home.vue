0c866448774ed3e344e6c61940ab59dd572e872e
Nuttakorn Tanseng <gontanseng@gmail.com>
2021-03-25 15:57:24 +0700

    edit layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -36,9 +36,48 @@
         </v-chip>
       </v-col>
     </v-row>
-    <v-row class="mb-15" v-if="headlines">
+    <v-row class="mb-15 hidden-sm-and-down" v-if="headlines">
       <v-col
         cols="3"
+        v-for="(headline, index) in headlines.articles"
+        :key="index"
+        ><v-card class="mx-auto mt-10" color="brown lighten-5">
+          <v-img
+            :src="headline.urlToImage"
+            height="200px"
+            gradient=" rgba(0,0,0,0.2), rgba(0,0,0,0.4)"
+            @click="readMore(headline)"
+          >
+            <span class="my-span">
+              {{ headline.source.name }}
+            </span>
+          </v-img>
+
+          <v-card-title class="black--text" @click="readMore(headline)">
+            <h5 v-if="headline.title" class="font-weight-medium">
+              {{ headline.title.slice(0, 70) }}..
+            </h5>
+          </v-card-title>
+          <v-card-subtitle
+            class="text black--text font-weight-light"
+            v-if="headline"
+          >
+            {{ headline.publishedAt.split("T")[0] }}
+          </v-card-subtitle>
+          <v-spacer></v-spacer>
+          <v-card-actions>
+            <v-btn color="blue lighten-2" text @click="readMore(headline)">
+              Detail
+            </v-btn>
+            <v-spacer></v-spacer>
+            <v-icon @click="editTitle(index)">mdi-dots-vertical</v-icon>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row class="mb-15 hidden-md-and-up" v-if="headlines" justify="center">
+      <v-col
+        cols="10"
         v-for="(headline, index) in headlines.articles"
         :key="index"
         ><v-card class="mx-auto mt-10" color="brown lighten-5">
